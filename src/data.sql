76e5b025643fc54a6266a52b9c63a15acaec5c5a
Jaime <jgomezm@tecsup.edu.pe>
2021-07-22 19:19:42 -0500

    Iinit

---
+++ src/data.sql
@@ -0,0 +1,21 @@
+-- SELECCIONAR BASE DE DATOS
+USE inverntariodb;
+
+
+-- INSERTA REGISTROS
+INSERT INTO  categorias  (ID,  NOMBRE,  DESCRIPCION,  ORDEN  )  VALUES  (1,'Procesadores', 'Categoria de procesadores',1) ;
+INSERT INTO  categorias  (ID,  NOMBRE,  DESCRIPCION,  ORDEN  )  VALUES   (2,'Memorias', 'Categoria de memorias',2) ;
+INSERT INTO  categorias  (ID,  NOMBRE,  DESCRIPCION,  ORDEN  )  VALUES  (3,'Discos  Duros', 'Categoria de discos duros',3);
+
+INSERT INTO productos (id, categorias_id, nombre, descripcion,  precio, stock ) VALUES (1,1,'Intel  Core  I7','Procesador  Intel  Core  I7-8700  Lga  1151 8va',1480,6) ;
+INSERT INTO productos (id, categorias_id, nombre, descripcion,  precio, stock ) VALUES (2,2,'Kingstone','Kingstone 64GB DDR3',80.00,12); 
+INSERT INTO productos (id, categorias_id, nombre, descripcion,  precio, stock ) VALUES (12,1,'ihpone 9','Iphone ',2000.00,6);
+INSERT INTO productos (id, categorias_id, nombre, descripcion,  precio, stock ) VALUES (16,1,'Amd','Procesador AMD ',223.00,12);
+
+
+-- MOSTRAR INFO DE TABLAS
+
+SELECT * FROM categorias;
+
+SELECT * FROM productos;
+
